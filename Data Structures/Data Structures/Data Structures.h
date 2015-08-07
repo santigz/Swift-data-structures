d97deebff13b4cb014e720967d10a1775bb45ee6
sangonz <sangonz@gmail.com>
2015-08-07 21:36:44 +0200

    Empty project

---
+++ Data Structures/Data Structures/Data Structures.h
@@ -0,0 +1,19 @@
+//
+//  Data Structures.h
+//  Data Structures
+//
+//  Created by Santiago González on 07/08/2015.
+//  Copyright (c) 2015 sangonz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Data Structures.
+FOUNDATION_EXPORT double Data_StructuresVersionNumber;
+
+//! Project version string for Data Structures.
+FOUNDATION_EXPORT const unsigned char Data_StructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data_Structures/PublicHeader.h>
+
+
