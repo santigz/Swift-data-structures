f104e27bc091fdc0f35f8f813a2c80cbe7bdc8a7
sangonz <sangonz@gmail.com>
2015-08-26 11:55:32 +0200

    refactor test: assert to XCTAssert

--- DataStructures/DataStructuresTests/DataStructuresTests.swift
+++ DataStructures/DataStructuresTests/DataStructuresTests.swift
@@ -16,12 +16,12 @@ class DataStructuresTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        assert(testLength > 2, "lengthTest is too small")
+        XCTAssert(testLength > 2, "lengthTest is too small for testing")
     }
     
     func doubleEndedContainerEmptyTest<T: DoubleEndedContainer where T.Generator.Element == Int>(inout container: T) {
         XCTAssertTrue(container.isEmpty)
-        XCTAssertTrue(container.count == 0)
+        XCTAssertEqual(container.count, 0)
         XCTAssertNil(container.back)
         XCTAssertNil(container.front)
         XCTAssertNil(container.popBack())
@@ -36,7 +36,7 @@ class DataStructuresTests: XCTestCase {
     
     func doubleEndedContainerFullTest<T: DoubleEndedContainer where T.Generator.Element == Int>(inout container: T) {
         XCTAssertFalse(container.isEmpty)
-        XCTAssertTrue(container.count == testLength)
+        XCTAssertEqual(container.count, testLength)
         XCTAssertNotNil(container.back)
         XCTAssertNotNil(container.front)
         
@@ -59,20 +59,20 @@ class DataStructuresTests: XCTestCase {
         let testVal = testLength * 2
         
         container.back = nil
-        XCTAssertTrue(container.count == testLength - 1)
+        XCTAssertEqual(container.count, testLength - 1)
         container.pushBack(testVal)
-        XCTAssertTrue(container.back == testVal)
+        XCTAssertEqual(container.back, testVal)
         container.back = back
         XCTAssertEqual(container.back, back)
-        XCTAssertTrue(container.count == testLength)
+        XCTAssertEqual(container.count, testLength)
         
         container.front = nil
-        XCTAssertTrue(container.count == testLength - 1)
+        XCTAssertEqual(container.count, testLength - 1)
         container.pushFront(testVal)
-        XCTAssertTrue(container.front == testVal)
+        XCTAssertEqual(container.front, testVal)
         container.front = front
         XCTAssertEqual(container.front, front)
-        XCTAssertTrue(container.count == testLength)
+        XCTAssertEqual(container.count, testLength)
     }
     
     /**
@@ -84,10 +84,10 @@ class DataStructuresTests: XCTestCase {
         
         for element in 1...testLength {
             container.pushBack(element)
-            assert(container.count == element, "Container should have \(element) elements")
-            assert(!container.isEmpty, "Container should not be empty")
-            assert(container.back == element, "Bad front")
-            assert(container.front == 1, "Bad back")
+            XCTAssertEqual(container.count, element, "Container should have \(element) elements")
+            XCTAssertNotNil(container.isEmpty, "Container should not be empty")
+            XCTAssertEqual(container.back, element, "Bad back")
+            XCTAssertEqual(container.front, 1, "Bad front")
         }
         
         doubleEndedContainerFullTest(&container)
@@ -104,10 +104,10 @@ class DataStructuresTests: XCTestCase {
         for element in Array((1...testLength).reverse()) {
             container.pushFront(element)
             let count = testLength - element + 1
-            assert(container.count == count, "Container should have \(count) elements")
-            assert(!container.isEmpty, "Container should not be empty")
-            assert(container.back == testLength, "Bad back")
-            assert(container.front == element, "Bad front")
+            XCTAssertEqual(container.count, count, "Container should have \(count) elements")
+            XCTAssertNotNil(container.isEmpty, "Container should not be empty")
+            XCTAssertEqual(container.back, testLength, "Bad back")
+            XCTAssertEqual(container.front, element, "Bad front")
         }
         
         doubleEndedContainerFullTest(&container)
@@ -122,13 +122,13 @@ class DataStructuresTests: XCTestCase {
         
         for element in Array((2...testLength).reverse()) {
             let popped = container.popBack()
-            assert(popped! == element, "Bad popFront() element")
-            assert(container.count == element - 1, "Container should have \(element) elements")
-            assert(!container.isEmpty, "Container should not be empty")
-            assert(container.back == element - 1, "Bad back")
-            assert(container.front == 1, "Bad front")
+            XCTAssertEqual(popped!, element, "Bad popFront() element")
+            XCTAssertEqual(container.count, element - 1, "Container should have \(element) elements")
+            XCTAssertNotNil(container.isEmpty, "Container should not be empty")
+            XCTAssertEqual(container.back, element - 1, "Bad back")
+            XCTAssertEqual(container.front, 1, "Bad front")
         }
-        assert(container.popBack() == 1, "Bad popFront() element")
+        XCTAssertEqual(container.popBack(), 1, "Bad popFront() element")
         doubleEndedContainerEmptyTest(&container)
     }
     
@@ -141,14 +141,14 @@ class DataStructuresTests: XCTestCase {
         
         for element in 1...(testLength - 1) {
             let popped = container.popFront()
-            assert(popped! == element, "Bad popFront() element")
+            XCTAssert(popped! == element, "Bad popFront() element")
             let revelement = testLength - element
-            assert(container.count == revelement, "Container should have \(revelement) elements")
-            assert(!container.isEmpty, "Container should not be empty")
-            assert(container.back == testLength, "Bad back")
-            assert(container.front == element + 1, "Bad front")
+            XCTAssertEqual(container.count, revelement, "Container should have \(revelement) elements")
+            XCTAssertNotNil(container.isEmpty, "Container should not be empty")
+            XCTAssertEqual(container.back, testLength, "Bad back")
+            XCTAssertEqual(container.front, element + 1, "Bad front")
         }
-        assert(container.popFront() == testLength, "Bad popFront() element")
+        XCTAssertEqual(container.popFront(), testLength, "Bad popFront() element")
         doubleEndedContainerEmptyTest(&container)
     }
     
@@ -159,13 +159,13 @@ class DataStructuresTests: XCTestCase {
         
         var i: Int = 1
         for element in container {
-            assert(element == i, "Bad DoubleEndedContainer subscript")
+            XCTAssertEqual(element, i, "Bad DoubleEndedContainer subscript")
             ++i
         }
         
         let pos = testLength / 2
         container[pos] = 34
-        assert(container[pos] == 34, "LinkedList subscript failed")
+        XCTAssertEqual(container[pos], 34, "LinkedList subscript failed")
         
         container.removeAll()
     }
@@ -177,8 +177,8 @@ class DataStructuresTests: XCTestCase {
         for _ in 1...min(3, testLength) {
             container.pushFront(1)
         }
-        assert(container.count > 0, "Count should be zero")
-        assert(!container.isEmpty, "Container should not be empty")
+        XCTAssert(container.count > 0, "Count should be greater than zero")
+        XCTAssertNotNil(container.isEmpty, "Container should not be empty")
         container.removeAll()
         doubleEndedContainerEmptyTest(&container)
     }
@@ -190,8 +190,8 @@ class DataStructuresTests: XCTestCase {
         This also tests Queue, Deque and Stack, which are a subset of DoubleEndedContainer
      */
     func doubleEndedContainerTest<T: DoubleEndedContainer where T.Generator.Element == Int>(inout container: T) {
-        assert(container.count == 0, "Count should be zero")
-        assert(container.isEmpty, "Container should be empty")
+        XCTAssertEqual(container.count, 0, "Count should be zero")
+        XCTAssert(container.isEmpty, "Container should be empty")
         
         // Try all combinations of push/pop back/front
         doubleEndedContainerPushBackTest(&container)
@@ -212,15 +212,12 @@ class DataStructuresTests: XCTestCase {
     }
     
     func queueTest<T: QueueType where T.Generator.Element == Int>(inout queue: T) {
-        assert(queue.count == 0, "Count should be zero")
-        assert(queue.isEmpty, "Container should be empty")
+        XCTAssert(queue.count == 0)
+        XCTAssert(queue.isEmpty)
         
-        assert(queue.front == nil, "Front should be nil")
-        assert(queue.back == nil, "Back should be nil")
-        assert(queue.popFront() == nil, "Front should be nil")
-        
-        
-        
+        XCTAssertNil(queue.front)
+        XCTAssertNil(queue.back)
+        XCTAssertNil(queue.popFront())
     }
     
     func testQueue() {
@@ -229,8 +226,8 @@ class DataStructuresTests: XCTestCase {
     
     func testCircularArray() {
         var circarray = CircularArray<Int>(capacity: testLength)
-        assert(!circarray.isFull, "BidirectionalContainer should not be full")
-        assert(circarray.capacity == testLength, "Bad BidirectionalContainer capacity")
+        XCTAssertNotNil(circarray.isFull)
+        XCTAssert(circarray.capacity == testLength)
         
         doubleEndedContainerTest(&circarray)
         
@@ -241,16 +238,16 @@ class DataStructuresTests: XCTestCase {
         
         var i = 1
         for element in circarray {
-            assert(element == i, "Bad CircularArray element while looping")
+            XCTAssert(element == i, "Bad CircularArray element while looping")
             ++i
         }
 
         let pos = testLength / 2
         circarray[pos] = 34
-        assert(circarray[pos] == 34, "CircularArray subscript failed")
+        XCTAssert(circarray[pos] == 34, "CircularArray subscript failed")
         
-        assert(circarray.isFull, "CircularArray should be full")
-        assert(circarray.capacity == testLength, "Bad CircularArray capacity")
+        XCTAssert(circarray.isFull)
+        XCTAssert(circarray.capacity == testLength)
     }
     
     func testCircularArrayPerformance() {
@@ -285,7 +282,7 @@ class DataStructuresTests: XCTestCase {
         }
 
         llist2.removeAll()
-        assert(nElementsDeinitialized == testLength, "Wrong number of deinitializers")
+        XCTAssert(nElementsDeinitialized == testLength, "Wrong number of deinitializers")
     }
     
     func testLinkedListPerformance() {
